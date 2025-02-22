f919dd6c285dca22c0672b67b29688d6224ad949
merlicne <phannakon.p@kkumail.com>
2025-02-23 03:33:48 +0700

    refactor ResearchGroupController.php for more readable

--- app/Http/Controllers/ResearchGroupController.php
+++ app/Http/Controllers/ResearchGroupController.php
@@ -18,7 +18,7 @@ class ResearchGroupController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    function __construct()
+    public function __construct()
     {
         $this->middleware('permission:groups-list|groups-create|groups-edit|groups-delete', ['only' => ['index', 'show']]);
         $this->middleware('permission:groups-create', ['only' => ['create', 'store']]);
@@ -33,6 +33,7 @@ class ResearchGroupController extends Controller
         return view('research_groups.index', compact('researchGroups'));
     }
 
+    // Create new Research Group Form
     public function create()
     {
         $user = auth()->user();
@@ -46,7 +47,8 @@ class ResearchGroupController extends Controller
         $authors = Author::get();
         return response()->view('research_groups.create', compact('users', 'funds', 'authors'));
     }
-
+    
+    // Create new Research Group Trigger
     public function store(Request $request)
     {
         $request->validate([
@@ -55,40 +57,24 @@ class ResearchGroupController extends Controller
             'head' => 'required',
             //'group_image' => 'required|mimes:png,jpg,jpeg|max:2048',
         ]);
-
         if ($this->isDuplicateUserInRequest($request)) {
             return redirect()->back()->with('error', 'Duplicate user in request');
         }
-
         $input = $request->all();
         $researchGroup = ResearchGroup::create($input);
-        if ($request->group_image) {
-            if (!$this->isFileExtensionValid($request->group_image)) {
-                return redirect()->back()->with('error', 'Invalid file type');
-            }
-            $input['group_image'] = 'RG'. $researchGroup->id . '.' . $request->group_image->extension();
-            $request->group_image->move(public_path('img'), $input['group_image']);
-        }
+        $input['group_image'] = $this->setImage($request, $researchGroup);
         $researchGroup->update($input);
-        $head = $request->head;
-        $fund = $request->fund;
-        $researchGroup->user()->attach($head, ['role' => 1]);
-        if ($request->moreFields) {
-            foreach ($request->moreFields as $key => $value) {
-
-                if ($value['userid'] != null) {
-                    $researchGroup->user()->attach($value, ['role' => 2]);
-                }
-            }
-        }
+        $this->addUsersToResearchGroup($researchGroup, $request);
         return redirect()->route('researchGroups.index')->with('success', 'research group created successfully.');
     }
+
     public function show(ResearchGroup $researchGroup)
     {
         return view('research_groups.show', compact('researchGroup'));
     }
 
 
+    // Edit Research Group Form
     public function edit(ResearchGroup $researchGroup)
     {
         $researchGroup = ResearchGroup::find($researchGroup->id);
@@ -98,43 +84,27 @@ class ResearchGroupController extends Controller
         $authors = Author::get();
         return view('research_groups.edit', compact('researchGroup', 'users', 'authors'));
     }
+
+    // Edit Research Group Trigger
     public function update(Request $request, ResearchGroup $researchGroup)
     {
         $request->validate([
             'group_name_th' => 'required',
             'group_name_en' => 'required',
         ]);
-
         if ($this->isDuplicateUserInRequest($request)) {
             return redirect()->back()->with('error', 'Duplicate user in request');
         }
-
         $input = $request->all();
-        if ($request->group_image) {
-            if (!$this->isFileExtensionValid($request->group_image)) {
-                return redirect()->back()->with('error', 'Invalid file type');
-            }
-            $input['group_image'] = 'RG'. $researchGroup->id . '.' . $request->group_image->extension();
-            $request->group_image->move(public_path('img'), $input['group_image']);
-        }
+        $input['group_image'] = $this->setImage($request, $researchGroup);
         $researchGroup->update($input);
-        $head = $request->head;
         $researchGroup->user()->detach();
-        $researchGroup->user()->attach($head, ['role' => 1]);
-
-
-
-        if ($request->moreFields) {
-            foreach ($request->moreFields as $key => $value) {
-
-                if ($value['userid'] != null) {
-                    $researchGroup->user()->attach($value, ['role' => 2]);
-                }
-            }
-        }
+        $this->addUsersToResearchGroup($researchGroup, $request);
         return redirect()->route('researchGroups.index')
             ->with('success', 'researchGroups updated successfully');
     }
+
+    // Delete Research Group
     public function destroy(ResearchGroup $researchGroup)
     {
         $this->authorize('delete', $researchGroup);
@@ -145,7 +115,10 @@ class ResearchGroupController extends Controller
 
     private function isDuplicateUserInRequest(Request $request){
         $users = [];
-        foreach ($request->moreFields as $key => $value) {
+        if ($request->moreFields == null) {
+            return false;
+        }
+        foreach ($request->moreFields as $value) {
             if ($value['userid'] != null) {
                 if (in_array($value['userid'], $users) || $value['userid'] == $request->head) {
                     return true;
@@ -156,9 +129,41 @@ class ResearchGroupController extends Controller
         return false;
     }
 
+    
+    private function setImage($request, $researchGroup){
+        $fileName = '';
+        if ($request->group_image) {
+            if (!$this->isFileExtensionValid($request->group_image)) {
+                return redirect()->back()->with('error', 'Invalid file type');
+            }
+            $fileName = 'RG'. $researchGroup->id . '.' . $request->group_image->extension();
+            $request->group_image->move(public_path('img'), $fileName);
+        }else{
+            if ($researchGroup->group_image) {
+                $fileName = $researchGroup->group_image;
+            }else{
+                $fileName = 'img.jpg';
+            }
+        }
+        \Log::info('Image uploaded ' . $fileName);
+        return $fileName;
+        
+    }
+    
     private function isFileExtensionValid($file){
         $permitted_extensions = array('jpg', 'jpeg', 'png', 'gif', 'svg', 'webp', 'bmp');
         return in_array($file->extension(), $permitted_extensions);
+    }
+    private function addUsersToResearchGroup($researchGroup, $request){
+        $head = $request->head;
+        $researchGroup->user()->attach($head, ['role' => 1]);
+        if ($request->moreFields) {
+            foreach ($request->moreFields as $value) {
+                if ($value['userid'] != null) {
+                    $researchGroup->user()->attach($value, ['role' => 2]);
+                }
+            }
+        }
     }
 }
 
